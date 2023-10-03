d02ccf16aadfcf3028cdc8cad0a6214dfe2e6364
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-10-03 09:44:13 +0200

    Header created

---
+++ app/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <>
+        <header className="flex items-center justify-evenly">
+            <div className="flex items-center">
+        <img src="/assets/Logo.png" alt="" />
+    </div>
+
+    </header>
+   
+        </>
+      );
+}
+ 
+export default Header;
